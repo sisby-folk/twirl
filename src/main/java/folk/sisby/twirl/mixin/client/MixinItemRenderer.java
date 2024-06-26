9645fecf63edab777ade74e8a4c025d3779056ee
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-06-26 10:26:55 +1000

    use wrapmethod

--- src/main/java/folk/sisby/twirl/mixin/client/MixinItemRenderer.java
+++ src/main/java/folk/sisby/twirl/mixin/client/MixinItemRenderer.java
@@ -1,5 +1,7 @@
 package folk.sisby.twirl.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import folk.sisby.twirl.Twirl;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -11,33 +13,23 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.RotationAxis;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemRenderer.class)
 public class MixinItemRenderer {
-    @Inject(method = "renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/world/World;III)V", at = @At("HEAD"))
-    public void renderItemHead(
-        LivingEntity entity, ItemStack item, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int light, int overlay, int seed, CallbackInfo ci
-    ) {
-        matrices.push();
-        if (entity == null) return;
+    @WrapMethod(method = "renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/world/World;III)V")
+    public void renderItemHead(LivingEntity entity, ItemStack item, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int light, int overlay, int seed, Operation<Void> original) {
         int twirlingLevel = Twirl.getLevelIn(item, Twirl.TWIRLING);
-        boolean mainHand = entity.getActiveItem() == item;
-        if (twirlingLevel > 0 && entity.isUsingItem() && (mainHand || Twirl.getLevelIn(entity.getActiveItem(), Twirl.TWIRLING) > 0)) {
+        if (entity != null && twirlingLevel > 0 && entity.isUsingItem() && (entity.getActiveItem() == item || Twirl.getLevelIn(entity.getActiveItem(), Twirl.TWIRLING) > 0)) {
+            matrices.push();
             float tickDelta = MinecraftClient.getInstance().getRenderTickCounter().getTickDelta(false);
             matrices.translate(leftHanded ? -0.1f : 0.1f, 0.0f, 0.0f);
             matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(leftHanded ? -15f : 15f));
-            RotationAxis axis = item.isIn(Twirl.ROTATE_Z) && mainHand ? RotationAxis.NEGATIVE_Z : RotationAxis.NEGATIVE_X;
+            RotationAxis axis = item.isIn(Twirl.ROTATE_Z) && entity.getActiveItem() == item ? RotationAxis.NEGATIVE_Z : RotationAxis.NEGATIVE_X;
             matrices.multiply(axis.rotationDegrees(((float) entity.getItemUseTime() + tickDelta) * twirlingLevel * Twirl.TWIRL_SPEED));
+            original.call(entity, item, renderMode, leftHanded, matrices, vertexConsumers, world, light, overlay, seed);
+            matrices.pop();
+        } else {
+            original.call(entity, item, renderMode, leftHanded, matrices, vertexConsumers, world, light, overlay, seed);
         }
-    }
-
-    @Inject(method = "renderItem(Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/json/ModelTransformationMode;ZLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/world/World;III)V", at = @At("TAIL"))
-    public void renderItemTail(
-        LivingEntity entity, ItemStack item, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, World world, int light, int overlay, int seed, CallbackInfo ci
-    ) {
-        matrices.pop();
     }
 }
